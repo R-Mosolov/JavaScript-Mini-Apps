017f14767a9c702a99e7e74d12b1ffce48b4a16b
R-Mosolov <r.v.mosolov@ya.ru>
2020-02-20 00:33:34 +0300

    #15@0h50m: reduce syntax by creating new functions

--- cohort-analysis/react/src/components/simple-component.jsx
+++ cohort-analysis/react/src/components/simple-component.jsx
@@ -3,49 +3,28 @@ import { data } from "./data";
 
 class SimpleComponent extends React.Component {
     render() {
-        function markContrastMedian(item) {
-            const max = Math.max(...data.median);
-            const min = Math.min(...data.median);
+        function markContrastValues(item, maxValue, minValue) {
+            const max = maxValue;
+            const min = minValue;
 
             if (item === max) {
                 return <li className="list-unstyled bg-success">{item}</li>;
             } else if (item === min) {
                 return <li className="list-unstyled bg-danger">{item}</li>;
             }
-            return <li className="list-unstyled bg-light">{item}</li>;
-        }
-        function markContrastMode(item) {
-            const max = Math.max(...data.mode);
-            const min = Math.min(...data.mode);
 
-            if (item === max) {
-                return <li className="list-unstyled bg-success">{item}</li>;
-            } else if (item === min) {
-                return <li className="list-unstyled bg-danger">{item}</li>;
-            }
-            return <li className="list-unstyled bg-light">{item}</li>;
-        }
-        function markContrastSummary(item) {
-            const max = Math.max(...data.summary);
-            const min = Math.min(...data.summary);
-
-            if (item === max) {
-                return <li className="list-unstyled bg-success">{item}</li>;
-            } else if (item === min) {
-                return <li className="list-unstyled bg-danger">{item}</li>;
-            }
             return <li className="list-unstyled bg-light">{item}</li>;
         }
 
-        const renderMedian = data.median.map(itemValue => {
-            return markContrastMedian(itemValue);
-        });
-        const renderMode = data.mode.map(itemValue => {
-            return markContrastMode(itemValue);
-        });
-        const renderSummary = data.summary.map(itemValue => {
-            return markContrastSummary(itemValue);
-        });
+        const renderMedian = data.median.map(itemValue => markContrastValues(
+                `${itemValue}`, `${Math.max(...data.median)}`, `${Math.min(...data.median)}`)
+        );
+        const renderMode = data.mode.map(itemValue => markContrastValues(
+            `${itemValue}`, `${Math.max(...data.mode)}`, `${Math.min(...data.mode)}`)
+        );
+        const renderSummary = data.summary.map(itemValue => markContrastValues(
+            `${itemValue}`, `${Math.max(...data.summary)}`, `${Math.min(...data.summary)}`)
+        );
 
         return (
             <div className="mt-5 border rounded">
