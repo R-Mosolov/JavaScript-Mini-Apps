bc5d037fb94a13f628e11fad471642426eb78528
Roman Mosolov <R.V.Mosolov@ya.ru>
2020-06-08 20:45:15 +0300

    #44@1h21m: add deleting of error duplicates to searcher module

--- zakyat-searcher/src/components/header/services/render-advices.js
+++ zakyat-searcher/src/components/header/services/render-advices.js
@@ -2,14 +2,39 @@ function renderAdvices(suggestionList, error) {
     const suggestionListLength = suggestionList.length;
     const searchModalWindow = document.getElementById("search-modal-window");
     const isError = (error !== "");
+    let adviceCounter = 0;
+
+    // Deleting repeated alerts
+    for (let i = 0; i <= suggestionListLength; i++) {
+        const adviceById = document.getElementById(`advice-${i}`);
+        console.log(`adviceById: ${adviceById}`);
+
+        if (adviceById) {
+            adviceById.remove();
+            console.log(`Following ID has removed: ${adviceById}`);
+        }
+    }
 
     if (suggestionListLength > 0) {
-        let adviceField = document.createElement("div");
-        adviceField.innerHTML = suggestionList[0];
-        adviceField.className = "m-0 mt-2 alert";
+        for (let suggestion in suggestionList) {
+            adviceCounter++;
 
-        if (adviceField) {
-            searchModalWindow.after(adviceField);
+            let adviceField = document.createElement("div");
+            adviceField.innerText = suggestionList[suggestion];
+            adviceField.className = `m-0 mt-2 alert`;
+            adviceField.id = `advice-${adviceCounter}`;
+
+            // Inserting the alert into GUI
+            if (adviceField) {
+                searchModalWindow.after(adviceField);
+            }
+
+            // Deleting repeated alerts
+            const errorFieldById = document.getElementById("search-error");
+
+            if (errorFieldById) {
+                errorFieldById.remove();
+            }
         }
     }
 
@@ -17,7 +42,16 @@ function renderAdvices(suggestionList, error) {
         let errorField = document.createElement("div");
         errorField.innerHTML = error;
         errorField.className = "m-0 mt-2 alert";
+        errorField.id = "search-error";
 
+        // Deleting repeated alerts
+        const errorFieldById = document.getElementById("search-error");
+
+        if (errorFieldById) {
+            errorFieldById.remove();
+        }
+
+        // Rendering error
         searchModalWindow.after(errorField);
     }
 }
